c13ab5a42ad07d3ce9f0e9263b27e44995f5bf66
Mai Thanh <maiduyhungthanh@gmail.com>
2021-06-23 16:58:17 +0700

    ok

---
+++ src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args)  {
+     String tên = "Thành";
+     int tuổi = 28 ;
+     String address = "Nghệ An";
+            System.out.println("My name is "+tên + " ,"+"Iam " +tuổi +" year old");
+            System.out.println("I from in "+address);
+            System.out.println("----------------------------------------------");
+        // Calculator
+        Calculator cal = new Calculator();
+        int one = cal.cong();
+        int two = cal.tru();
+        int three = cal.nhan();
+        float four = cal.chia();
+
+        
+        System.out.println("ĐS phép cộng = " +one);
+        System.out.println("ĐS phép trừ  = " +two);
+        System.out.println( "ĐS phép nhân = " +three);
+        System.out.println( "ĐS phép chia = " +four);
+   }
+}
---
+++ src/Calculator.java
@@ -0,0 +1,27 @@
+public class Calculator {
+    int num1 = 5;
+    int num2 = 10;
+
+    
+
+    public int cong() {
+        int a = num1 + num2;
+        return a;
+    }
+
+    public int tru() {
+        int a = num1 - num2;
+        return a;
+    }
+
+    public int nhan() {
+        int a = num1 * num2;
+        return a;
+    }
+
+    public float chia() {
+        float a = (float) num1 / num2;
+        return a;
+    }
+
+}
